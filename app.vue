59e44ff9b011a43a1861dacfee4700897c82cd2a
admin <sarvarikvarvarik@gmail.com>
2025-04-12 19:07:19 +0500

    зум

--- app.vue
+++ app.vue
@@ -13,3 +13,15 @@
     <p class="text-xs text-gray-500 mt-6">Powered by “U Holding”</p>
   </div>
 </template>
+
+
+<script setup>
+useHead({
+  meta: [
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+    }
+  ]
+})
+</script>
